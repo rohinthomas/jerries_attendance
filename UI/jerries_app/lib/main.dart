1538cf7d21506364e776d8dbe7a4785029258a97
rohin.thomas <rohin.thomas@innovaturelabs.com>
2024-02-08 10:23:25 +0530

    initial setup for ui_1

--- UI/jerries_app/lib/main.dart
+++ UI/jerries_app/lib/main.dart
@@ -1,3 +1,13 @@
+import 'package:flutter/material.dart';
+
 void main(){
-  
+ runApp(const Jerries());
+}
+class Jerries extends StatelessWidget {
+  const Jerries({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
